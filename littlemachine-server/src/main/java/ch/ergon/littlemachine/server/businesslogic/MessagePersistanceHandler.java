bc0286fefb0fba3e50efdcd08e6f1e27ce4d7a45
Stanislas Nanchen <stanislas.nanchen@ergon.ch>
2014-05-12 22:54:36 +0200

    add rough MapDB database for storing rawMessages

--- littlemachine-server/src/main/java/ch/ergon/littlemachine/server/businesslogic/MessagePersistanceHandler.java
+++ littlemachine-server/src/main/java/ch/ergon/littlemachine/server/businesslogic/MessagePersistanceHandler.java
@@ -1,14 +1,28 @@
 package ch.ergon.littlemachine.server.businesslogic;
 
+import java.io.File;
+
+import org.mapdb.BTreeMap;
+import org.mapdb.DB;
+import org.mapdb.DBMaker;
+
 import ch.ergon.littlemachine.server.message.Message;
 
 import com.lmax.disruptor.EventHandler;
 
 public class MessagePersistanceHandler implements EventHandler<Message> {
 
+	private final DB db = DBMaker.newFileDB(new File("/tmp/littlemachine")).closeOnJvmShutdown().make();
+	private final BTreeMap<Long, byte[]> messages;
+
+	public MessagePersistanceHandler() {
+		messages = db.getTreeMap("messages");
+	}
+
 	@Override
 	public void onEvent(Message event, long sequence, boolean endOfBatch) throws Exception {
-		System.out.println("we should log in MapDB");
+		messages.put(sequence, event.rawMessage);
+		db.commit();
 	}
 
 }
